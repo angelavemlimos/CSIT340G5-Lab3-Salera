2341c7cb747b5959d30476801023b96e9910263e
angelavemlimos <angelavem.limos@cit.edu>
2026-09-22 12:01:18 +0800

    1.4: course information step 4

--- src/App.jsx
+++ src/App.jsx
@@ -13,15 +13,16 @@ const Part = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <Part part={props.part1} />
-      <Part part={props.part2} />
-      <Part part={props.part3} />
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
     </div> 
   )
 }
 
 const Total = (props) => {
-  return <p> Total number of units: {props.total} </p>
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+  return <p> Total number of units: {total} </p>
 }
 
 const Footer = (props) => {
@@ -35,18 +36,20 @@ const Footer = (props) => {
 
 const App = () => {
   const course = 'BS Information Technology'
-  const part1 = {
-    name: 'CSIT340 - Industry Elective 1',
-    exercises: 3 
-  }
-  const part2 = {
-    name: 'CSIT321 - Applications Development and Emerging Technologies',
-    exercises: 3
-  }
-  const part3 = {
-    name: 'CSIT365 - Data Analytics 1',
-    exercises: 3
-  }
+  const parts = [
+    {
+      name: 'CSIT340 - Industry Elective 1',
+      exercises: 3
+    },
+    {
+      name: 'CSIT321 - Applications Development and Emerging Technologies',
+      exercises: 3
+    },
+    {
+      name: 'CSIT365 - Data Analytics 1',
+      exercises: 3
+    }
+  ]
 
   const studentName = 'Angela Vem Limos'
   const courseCode = 'CSIT340'
@@ -55,8 +58,8 @@ const App = () => {
   return (
     <div>
       <Header course = {course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Content parts={parts} />
+      <Total parts={parts} />
       <Footer fullName={studentName} courseCode={courseCode} section={section} />
      </div> 
   )
